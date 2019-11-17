dcd7382444e5a0b55656e9113c580822c8412b42
Nick R <nick_rister@yahoo.com>
2019-11-17 14:10:33 -0600

    Done

--- Personal Computer Build Configurator/src/application/Main.java
+++ Personal Computer Build Configurator/src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			//first comment
+		
 		} catch(Exception e) {
 			e.printStackTrace();
 		}

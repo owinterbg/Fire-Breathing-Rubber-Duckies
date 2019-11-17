14141c47dd7dc08cbe97310beaf7e206514ae75c
Nick R <nick_rister@yahoo.com>
2019-11-17 16:58:14 -0600

    made main controller

---
+++ Personal Computer Build Configurator/src/application/MainController.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+public class MainController {
+	
+	@FXML
+	private ChoiceBox<String> firstChoice;
+	@FXML
+	private ChoiceBox<String> secondChoice;
+	@FXML
+	private ChoiceBox<String> thirdChoice;
+	
+	public void start()
+	{
+		
+	}
+	
+	public void handle(ActionEvent handle) throws IOException
+	{
+		
+		
+		
+	}
+
+}

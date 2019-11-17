7bc1925d0695475dd2f1acdb3634a8467a12b562
Nick R <nick_rister@yahoo.com>
2019-11-17 17:10:37 -0600

    changed

--- Personal Computer Build Configurator/src/application/Main.java
+++ Personal Computer Build Configurator/src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,8 +12,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Main.fxml")); //link FXML document
+			//BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,600,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();

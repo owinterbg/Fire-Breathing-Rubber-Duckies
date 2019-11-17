314871392ffb84ee20783dcc9194f5b0fab1177e
Nick R <nick_rister@yahoo.com>
2019-11-17 15:56:32 -0600

    made this

---
+++ Personal Computer Build Configurator/src/application/handle.java
@@ -0,0 +1,17 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+public class handle {
+	
+	@FXML
+	private ChoiceBox<String> firstChoice;
+	@FXML
+	private ChoiceBox<String> secondChoice;
+	@FXML
+	private ChoiceBox<String> thirdBox;
+	
+	
+
+}

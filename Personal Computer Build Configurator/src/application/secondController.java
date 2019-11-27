dc692f1f2d3a1ef784a50def73f6c08a0a60bf6f
Nick R <nick_rister@yahoo.com>
2019-11-27 15:00:49 -0600

    made second scene and second scene controller & fxml

---
+++ Personal Computer Build Configurator/src/application/secondController.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class secondController {
+	
+	
+	public void changeToFirstScene(ActionEvent event) throws IOException
+	{
+		Parent secondSceneParent = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		
+		Scene secondScene = new Scene(secondSceneParent);
+		
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+		window.setScene(secondScene);
+		window.show();
+	}
+	
+
+}

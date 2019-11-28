6db95984890bdff62ca389677fb83e7ddc4bb7f8
Nick R <nick_rister@yahoo.com>
2019-11-28 12:57:24 -0600

    formatted second scene...still rough draft

--- Personal Computer Build Configurator/src/application/secondController.java
+++ Personal Computer Build Configurator/src/application/secondController.java
@@ -3,13 +3,33 @@ package application;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 public class secondController {
+	
+	//"CPU", "RAM", "Graphics Card", "Tower Case", "Power Supply", "Motherboard", "SSD", "HDD"
+	@FXML
+	private TextArea CPUOut;
+	@FXML
+	private TextArea RAMOut;
+	@FXML
+	private TextArea graphicsCardOut;
+	@FXML
+	private TextArea towerCaseOut;
+	@FXML
+	private TextArea powerSupplyOut;
+	@FXML
+	private TextArea motherboardOut;
+	@FXML
+	private TextArea SSDOut;
+	@FXML
+	private TextArea HDDOut;
 	
 	
 	public void changeToFirstScene(ActionEvent event) throws IOException
@@ -22,6 +42,19 @@ public class secondController {
 		
 		window.setScene(secondScene);
 		window.show();
+	}
+	
+	@FXML
+	public void initialize() {
+	 	
+
+
+	}
+	
+	
+	public void displayInformation(ActionEvent event)
+	{
+		
 	}
 	
 

bede11a9514b9865b06c302054d40763b7855919
Cody Oncken <35882153+weasel42351@users.noreply.github.com>
2019-12-07 00:08:22 -0600

    Update Motherboard.java

--- Personal Computer Build Configurator/src/application/Motherboard.java
+++ Personal Computer Build Configurator/src/application/Motherboard.java
@@ -1,3 +1,10 @@
+/**
+* Gets the information from database (csv file)
+* and stores it in variables to be used
+* when displaying the Motherboard information
+* to the user.
+*/
+
 package application;
 
 public class Motherboard {
